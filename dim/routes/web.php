72f8675eb3f3f81ea9a31846f43e1f17ca9c60c4
abigailregina <reginadwan@gmail.com>
2024-10-15 14:00:38 +0700

    update

--- dim/routes/web.php
+++ dim/routes/web.php
@@ -9,3 +9,20 @@ Route::get('/', function () {
 Route::get('/landing', function () {
     return view('landing',['title' => 'landing page']);
 });
+
+Route::get('/fundraise', function () {
+    return view('fundraise',['title' => 'fundraise page']);
+});
+
+Route::get('/signin', function () {
+    return view('signin',['title' => 'signin page']);
+});
+
+// Route::get('/signup', function () {
+//     return view('signup',['title' => 'signup page']);
+// });
+
+// Route::get('/aboutus', function () {
+//     return view('aboutus',['title' => 'aboutus page']);
+// });
+
